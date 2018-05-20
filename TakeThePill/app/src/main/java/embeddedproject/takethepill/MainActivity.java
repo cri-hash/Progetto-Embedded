2bd14aab58c107b44ffaeb2601e069a7fc6aaf2c
ivaste <ivancich.stefano@gmail.com>
2018-05-20 12:32:51 +0200

    Aggiunta Icona dell'App

--- TakeThePill/app/src/main/java/embeddedproject/takethepill/MainActivity.java
+++ TakeThePill/app/src/main/java/embeddedproject/takethepill/MainActivity.java
@@ -1,11 +1,17 @@
 package embeddedproject.takethepill;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
@@ -27,6 +33,8 @@ import java.util.Locale;
 import java.util.TimeZone;
 
 import database.DatabaseHelper;
+
+import static android.app.Notification.EXTRA_NOTIFICATION_ID;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -75,8 +83,32 @@ public class MainActivity extends AppCompatActivity
         }
             // conto il numero di assunzioni da oggi in poi
             // Se <10 ne aggiungo 10
+            //.......
 
 
+
+        // NOTIFICHE
+        createNotificationChannel();
+
+        // Quando si clicca sulla notifica viene richiamato il MAIN
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this, "idCanale")
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentTitle("My notification")
+                .setContentText("Hello World!")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                // Set the intent that will fire when the user taps the notification
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+        // notificationId is a unique int for each notification that you must define
+        int notificationId=1;
+        notificationManager.notify(notificationId, mBuilder.build());
 
 
 
@@ -131,5 +163,21 @@ public class MainActivity extends AppCompatActivity
         fragmentTransaction.commit();
     }
 
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "nomeCanale";
+            String description = "Descriione canale";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel("idCanale", name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
 
 }
