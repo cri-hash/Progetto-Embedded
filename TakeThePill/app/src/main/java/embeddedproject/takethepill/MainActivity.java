3e2338bccb0ab792683a6d64e033a636aafb60a4
ivaste <ivancich.stefano@gmail.com>
2018-05-20 13:37:39 +0200

    Iniziate le notifiche

--- TakeThePill/app/src/main/java/embeddedproject/takethepill/MainActivity.java
+++ TakeThePill/app/src/main/java/embeddedproject/takethepill/MainActivity.java
@@ -66,6 +66,8 @@ public class MainActivity extends AppCompatActivity
         DatabaseHelper db=new DatabaseHelper(this); //per testare il db in fase di creazione
         db.popolaDB();
 
+
+
         // Terapie di durata Senza Fine
 
         // Cerco terapie con getDays==-1
@@ -88,29 +90,8 @@ public class MainActivity extends AppCompatActivity
 
 
         // NOTIFICHE
-        createNotificationChannel();
-
-        // Quando si clicca sulla notifica viene richiamato il MAIN
-        Intent intent = new Intent(this, MainActivity.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
-
-        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this, "idCanale")
-                .setSmallIcon(R.drawable.ic_launcher_background)
-                .setContentTitle("My notification")
-                .setContentText("Hello World!")
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                // Set the intent that will fire when the user taps the notification
-                .setContentIntent(pendingIntent)
-                .setAutoCancel(true);
-
-        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
-
-        // notificationId is a unique int for each notification that you must define
-        int notificationId=1;
-        notificationManager.notify(notificationId, mBuilder.build());
-
-
+        Intent i = new Intent(this, NotificheIntentService.class);
+        startService(i);
 
     }
 
@@ -162,22 +143,6 @@ public class MainActivity extends AppCompatActivity
         fragmentTransaction.replace(R.id.frame, fragment);
         fragmentTransaction.commit();
     }
-
-
-    private void createNotificationChannel() {
-        // Create the NotificationChannel, but only on API 26+ because
-        // the NotificationChannel class is new and not in the support library
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            CharSequence name = "nomeCanale";
-            String description = "Descriione canale";
-            int importance = NotificationManager.IMPORTANCE_DEFAULT;
-            NotificationChannel channel = new NotificationChannel("idCanale", name, importance);
-            channel.setDescription(description);
-            // Register the channel with the system; you can't change the importance
-            // or other notification behaviors after this
-            NotificationManager notificationManager = getSystemService(NotificationManager.class);
-            notificationManager.createNotificationChannel(channel);
-        }
-    }
+    
 
 }
