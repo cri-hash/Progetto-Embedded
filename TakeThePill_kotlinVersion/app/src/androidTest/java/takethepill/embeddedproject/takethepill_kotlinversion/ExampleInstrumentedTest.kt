b0f9211e51f7ad15bd93be2ed4a32adc584f43c8
cristianlazzarin <cristian.lazzarin7@gmail.com>
2018-05-22 18:35:58 +0200

    Kotlin Version

---
+++ TakeThePill_kotlinVersion/app/src/androidTest/java/takethepill/embeddedproject/takethepill_kotlinversion/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package takethepill.embeddedproject.takethepill_kotlinversion
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("takethepill.embeddedproject.takethepill_kotlinversion", appContext.packageName)
+    }
+}
---
+++ TakeThePill_kotlinVersion/app/src/main/java/takethepill/embeddedproject/takethepill_kotlinversion/AddEditDrugActivity.kt
@@ -0,0 +1,7 @@
+package takethepill.embeddedproject.takethepill_kotlinversion
+
+/**
+ * Created by Cristian on 22/05/2018.
+ */
+class AddEditDrugActivity {
+}
